3aa4391f65a310edbe637ae95b543a6d6c81c407
GaliaArjhond <galiaarjhond0@gmail.com>
2025-12-05 19:54:56 +0800

    done part2(Yung 8 direction iyusin)

--- Curse-Harvest/Assets/Scripts/PlayerControl.cs
+++ Curse-Harvest/Assets/Scripts/PlayerControl.cs
@@ -12,10 +12,16 @@ public class PlayerControl : MonoBehaviour
     private Vector2 moveInput;
     private Rigidbody2D rb;
 
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -30,17 +36,30 @@ public class PlayerControl : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput() 
     {
         moveInput = playerControls.Movement.Move.ReadValue<Vector2>();    
+
+        animator.SetFloat("moveX", moveInput.x);
+        animator.SetFloat("moveY", moveInput.y);
+
     }
 
     private void Move() 
     {
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        if (moveInput.x < 0)
+            spriteRenderer.flipX = true;
+        else if (moveInput.x > 0)
+            spriteRenderer.flipX = false;
     }
 
 }
